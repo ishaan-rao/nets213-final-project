7c25e609e0dad54b5ff339a1091e1e98d8aec912
Michael Song <michaelsong0@gmail.com>
2021-05-08 11:25:29 -0700

    adding county data and resizing the maps

--- web-app/src/pages/about.js
+++ web-app/src/pages/about.js
@@ -17,7 +17,7 @@ const About = () => {
             <h1>About this project</h1>
             <p>This project was done by David Li, Ishaan Rao, Michael Song, and Eric Zhang as a final project for Nets213 at UPenn.</p>
             <br/>
-            <p>The majority of this project (i.e. data collection, cleaning, and aggregation) was done in Python, and the website was built using JavaScript and React</p>
+            <p>The majority of this project (i.e. data collection, cleaning, and aggregation) was done in Python (with a little bit of Excel and Tableau), and the website was built using JavaScript and React</p>
         </div>
     )
 }
